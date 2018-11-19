9c2c60ad3190e15e2830c35dcb9f1b5d79330626
Marissa Beatty <MarissaBeatty@users.noreply.github.com>
2018-11-19 16:39:26 -0500

    added new project

--- portfolio.js
+++ portfolio.js
@@ -9,4 +9,28 @@
   homeButton.addEventListener('click', function(event) {
     header.scrollIntoView({behavior: "smooth"});
   });
+
+  // CHECK IF THE HEADER IS FULLY IN VIEW; IF NOT, ADD THE ARROWS TO 
+  // TAKE US TO THE PAGE TOP
+  window.addEventListener('scroll', function(event) {
+    var isInViewport = function(elem) {
+    var bounding = elem.getBoundingClientRect();
+    return (
+        // bounding.top >= 0 &&
+        // bounding.left >= 0 &&
+        // bounding.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.bottom >= 300 
+        // bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    )};
+	if (!isInViewport(header)) {
+		homeButton.classList.add('visible');
+		console.log(homeButton.classList);
+		// console.log('In the viewport!');
+	} else {
+	  console.log('Not in the viewport... whomp whomp');
+	  homeButton.classList.remove('visible');
+
+	}
+  });
+
 } ()) //end of IIFE
