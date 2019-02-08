4d02b2f486ec5fdd2120f2b00bdedecb213c4b44
Marissa Beatty <MarissaBeatty@users.noreply.github.com>
2019-02-08 15:16:09 -0500

    added nav menu, testimonials and updated portfolio section

--- portfolio.js
+++ portfolio.js
@@ -4,62 +4,55 @@
   var body = document.querySelector('#body');
   var homeButton = body.querySelector('#homeButton');
   var header =  body.querySelector('#header');
+  var navButton = body.querySelector('#menu-icon');
+  var elementPosition = $('nav').offset();
+  var intViewportWidth = window.innerWidth;
+
 
   // SEND USER TO TOP OF HOMEPAGE ON CLICK
+
+  navButton.addEventListener('click', function(event) {
+    navButton.classList.toggle('open');
+    });
+
+  function stickyNav() {
+    if($(window).scrollTop() > elementPosition.top){
+      if(intViewportWidth > 960) {
+          $('nav').addClass('sticky');
+        } else if(intViewportWidth <= 960){
+        }
+    } else {
+        $('nav').removeClass('sticky');
+    }    
+  };
+
+window.onresize = function() {    
+  intViewportWidth = window.document.body.clientWidth;
+}
+
+$(window).scroll(function(){
+  stickyNav();   
+});
+
+  $(window).resize(function(){
+    $(window).scroll(function(){
+      stickyNav();
+    });
+  });
+
+
+// SMOOTH SCROLLING FOR HOME BUTTON
   homeButton.addEventListener('click', function(event) {
-      // var bounding = body.getBoundingClientRect();
-      // console.log(bounding);
-      // var top = bounding.top;
-      // var top = Math.abs(bounding.top);
       var top = (window.pageYOffset || body.scrollTop)  - (body.clientTop || 0);
-      // console.log(body.scrollTop, "scrolltop")
-      // console.log(top);
-      // body.animate({ scrollTop: 0 }, 1);
-      // window.scrollTo(0,0);
-
-
       event.preventDefault();
             $('html, body').stop().animate({
                 'scrollTop':  $(body).offset().top 
             }, 800, 'swing', function () {
-                // window.location.hash = target;
             });
-
-      $(function(){$(".scroll").click(function(){$("html,body").animate({scrollTop:$(".the-top").offset().top},"2000");return false})})
-      
-
-
-
-
-
+      $(function(){$(".scroll").click(function(){$("html,body").animate({scrollTop:$(".the-top").offset().top},"800");return false})})
     });
 
-  // homeButton.addEventListener('click', function(event) {
-    // body.classList.add('scrollToTop');
-    // console.log(body.classList);
 
-    // header.scrollIntoView({behavior: "smooth"});
-    // var scrollTop = header.scrollTop;
-    // console.log(scrollTop);
-    // body.animate([
-//   // keyframes
-  // { transform: 'translateY(0)' }, 
-  // { transform: 'translateY(100%)' }
-// ], { 
-//   // timing options
-  // duration: 500,
-  // iterations: 1, 
-  // animation-fill-mode:forwards,
-  // reset: false
-// }); 
-    // return false;
-    // html,body").animate({scrollTop:$(".the-top").offset().top},"2000");return false})})
-
-
-
-
-
-  // });
 
   // CHECK IF THE HEADER IS FULLY IN VIEW; IF NOT, ADD THE ARROWS TO 
   // TAKE US TO THE PAGE TOP
